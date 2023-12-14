2ef41c195470dfe72a5844402aca8b1486475f3c
Joey <jsmalme@gmail.com>
2023-12-13 18:12:21 -0600

    add another cors

--- nx-workspace/apps/backend/pfbackend/pfbackend/settings.py
+++ nx-workspace/apps/backend/pfbackend/pfbackend/settings.py
@@ -162,5 +162,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
     "d20character.com",
-    "www.d20character.com"
+    "www.d20character.com",
+    "django.d20character.com"
 ]
